cbfe45e6d988ed0f0046365fd18e045932f8af9b
Michael <michaelgainyo@gmail.com>
2021-05-16 00:37:45 -0400

    Added shared styles

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { isDate } from "lodash";
 
+import "./scss/main.scss";
+
 export const IS_DEV = process.env.NODE_ENV !== "production";
 
 export const DOMAIN = IS_DEV ? `http://127.0.0.1:8000` : `${window.location.origin}`;
