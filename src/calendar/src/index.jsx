c48d2dc207fcab0c356c6edd9061ff0686b713a7
Michaël <michael.gainyo@gmail.com>
2022-01-09 20:00:45 -0500

    updated calendar

--- src/calendar/src/index.jsx
+++ src/calendar/src/index.jsx
@@ -1,13 +1,12 @@
-import React, { createContext, useMemo, useState } from "react";
-import PropTypes from "prop-types";
-import { ChevronLeft, ChevronRight } from "react-bootstrap-icons";
+import React, { useContext, createContext, useMemo, useState } from 'react';
+import PropTypes from 'prop-types';
+import isDate from 'lodash.isdate';
+import { ChevronLeft, ChevronRight } from 'react-bootstrap-icons';
 
-import "./style.scss";
-import { formatDate, getClassName, isRequired } from "@miq/utils";
+import './style.scss';
 
-import { dateEquals, dateIsToday, getMonthDates, getWeekDates, WEEKDAYS } from "./utils";
-import { useContext } from "react";
-import { isDate } from "lodash";
+import { formatDate, getClassName, isRequired } from '@miq/utils';
+import { dateEquals, dateIsToday, getMonthDates, getWeekDates, WEEKDAYS } from './utils';
 
 /**
  *
@@ -24,42 +23,40 @@ export const CalendarCtx = createContext();
  */
 
 const DateWrapper = (props) => {
-    const { view, dt, context = {} } = props;
+  const { view, dt, context = {} } = props;
 
-    // console.log(context);
+  // console.log(context);
 
-    const isToday = dateIsToday(dt);
-    const isCurrent = dateEquals(dt, context.selected);
+  const isToday = dateIsToday(dt);
+  const isCurrent = dateEquals(dt, context.selected);
 
-    const DateComponent = props.DateComponent || context.DateComponent;
+  const DateComponent = props.DateComponent || context.DateComponent;
 
-    if (DateComponent) return <DateComponent {...props} {...{ view, isToday, isCurrent }} />;
+  if (DateComponent) return <DateComponent {...props} {...{ view, isToday, isCurrent }} />;
 
-    const handleDateClick = (e) => {
-        const { onDateClick } = context;
-        if (!onDateClick) return;
+  const handleDateClick = (e) => {
+    const { onDateClick } = context;
+    if (!onDateClick) return;
 
-        onDateClick({ event: e, date: dt, view, isToday, isCurrent });
-    };
+    onDateClick({ event: e, date: dt, view, isToday, isCurrent });
+  };
 
-    return (
-        <div
-            className={getClassName(["day", isToday && "today", isCurrent && "current"])}
-            onClick={handleDateClick}
-            role="button"
-        >
-            {view === "week" && (
-                <div className="day-weekday">{formatDate(dt, { weekday: "short" })}</div>
-            )}
-            <div className="day-date">{dt.getDate()}</div>
-        </div>
-    );
+  return (
+    <div
+      className={getClassName(['day', isToday && 'today', isCurrent && 'current'])}
+      onClick={handleDateClick}
+      role="button"
+    >
+      {view === 'week' && <div className="day-weekday">{formatDate(dt, { weekday: 'short' })}</div>}
+      <div className="day-date">{dt.getDate()}</div>
+    </div>
+  );
 };
 
 DateWrapper.propTypes = {
-    view: PropTypes.string.isRequired,
-    dt: PropTypes.instanceOf(Date).isRequired,
-    context: PropTypes.object.isRequired,
+  view: PropTypes.string.isRequired,
+  dt: PropTypes.instanceOf(Date).isRequired,
+  context: PropTypes.object.isRequired,
 };
 
 /**
@@ -69,29 +66,27 @@ DateWrapper.propTypes = {
  */
 
 const Week = (props) => {
-    const view = props.view || "week";
+  const view = props.view || 'week';
 
-    const context = useContext(CalendarCtx);
-    const curWeek = props.curWeek || context.curWeek;
+  const context = useContext(CalendarCtx);
+  const curWeek = props.curWeek || context.curWeek;
 
-    if (!curWeek.dates) return null;
+  if (!curWeek.dates) return null;
 
-    return (
-        <div className="Week">
-            <div className="Dates">
-                {curWeek.dates.map((dt) => {
-                    return (
-                        <DateWrapper {...props} {...{ dt, view, context }} key={dt.toString()} />
-                    );
-                })}
-            </div>
-        </div>
-    );
+  return (
+    <div className="Week">
+      <div className="Dates">
+        {curWeek.dates.map((dt) => {
+          return <DateWrapper {...props} {...{ dt, view, context }} key={dt.toString()} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 Week.propTypes = {
-    view: PropTypes.string,
-    curWeek: PropTypes.object,
+  view: PropTypes.string,
+  curWeek: PropTypes.object,
 };
 
 /**
@@ -101,28 +96,28 @@ Week.propTypes = {
  */
 
 const Month = (props) => {
-    const view = "month";
-    const { curMonth = {} } = useContext(CalendarCtx);
+  const view = 'month';
+  const { curMonth = {} } = useContext(CalendarCtx);
 
-    if (!curMonth.weeks) return null;
+  if (!curMonth.weeks) return null;
 
-    return (
-        <div className="Month">
-            <Weekdays {...props} {...{ view, curMonth }} />
+  return (
+    <div className="Month">
+      <Weekdays {...props} {...{ view, curMonth }} />
 
-            <div className="Weeks">
-                {curMonth.weeks.map((week) => (
-                    <Week {...props} {...{ view }} curWeek={week} key={week.dates[0].toString()} />
-                ))}
-            </div>
-        </div>
-    );
+      <div className="Weeks">
+        {curMonth.weeks.map((week) => (
+          <Week {...props} {...{ view }} curWeek={week} key={week.dates[0].toString()} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 Month.propTypes = {
-    view: PropTypes.string,
-    curDate: PropTypes.instanceOf(Date),
-    // context: PropTypes.object.isRequired,
+  view: PropTypes.string,
+  curDate: PropTypes.instanceOf(Date),
+  // context: PropTypes.object.isRequired,
 };
 
 /**
@@ -130,73 +125,73 @@ Month.propTypes = {
  */
 
 const DatePickerToggler = (props) => {
-    const { renderToggler, TogglerComponent, ...rest } = props;
+  const { renderToggler, TogglerComponent, ...rest } = props;
 
-    if (renderToggler) {
-        return renderToggler(rest);
-    }
-    if (TogglerComponent) {
-        return <TogglerComponent {...rest} />;
-    }
-    return null;
+  if (renderToggler) {
+    return renderToggler(rest);
+  }
+  if (TogglerComponent) {
+    return <TogglerComponent {...rest} />;
+  }
+  return null;
 };
 
 const DatePickerFooter = (props) => {
-    const { renderFooter, FooterComponent, ...rest } = props;
+  const { renderFooter, FooterComponent, ...rest } = props;
 
-    if (renderFooter) {
-        return renderFooter(rest);
-    }
-    if (FooterComponent) {
-        return <FooterComponent {...rest} />;
-    }
-    return null;
+  if (renderFooter) {
+    return renderFooter(rest);
+  }
+  if (FooterComponent) {
+    return <FooterComponent {...rest} />;
+  }
+  return null;
 };
 
 const DatePicker = (props) => {
-    const { showBody = true } = props;
-    const hasToggler = props.renderToggler || props.TogglerComponent;
-    const hasFooter = props.renderFooter || props.FooterComponent;
-    const [show, setShow] = useState(showBody);
+  const { showBody = true } = props;
+  const hasToggler = props.renderToggler || props.TogglerComponent;
+  const hasFooter = props.renderFooter || props.FooterComponent;
+  const [show, setShow] = useState(showBody);
 
-    return (
-        <div className="miq-datepicker">
-            {hasToggler && (
-                <div className="miq-datepicker-header">
-                    <DatePickerToggler {...props} {...{ show, setShow }} />
-                </div>
-            )}
-
-            {show && (
-                <>
-                    <div className="miq-datepicker-body">
-                        <div className="miq-datepicker-content">
-                            <div className="miq-datepicker-toolbar">
-                                <Calendar.Prev view="month" />
-                                <Calendar.Title view="month" />
-                                <Calendar.Next view="month" />
-                            </div>
-                            <div className="miq-datepicker-month">
-                                <Calendar.Month />
-                            </div>
-
-                            {hasFooter && (
-                                <div className="miq-datepicker-footer">
-                                    <DatePickerFooter {...props} {...{ show, setShow }} />
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </>
-            )}
+  return (
+    <div className="miq-datepicker">
+      {hasToggler && (
+        <div className="miq-datepicker-header">
+          <DatePickerToggler {...props} {...{ show, setShow }} />
         </div>
-    );
+      )}
+
+      {show && (
+        <>
+          <div className="miq-datepicker-body">
+            <div className="miq-datepicker-content">
+              <div className="miq-datepicker-toolbar">
+                <Calendar.Prev view="month" />
+                <Calendar.Title view="month" />
+                <Calendar.Next view="month" />
+              </div>
+              <div className="miq-datepicker-month">
+                <Calendar.Month />
+              </div>
+
+              {hasFooter && (
+                <div className="miq-datepicker-footer">
+                  <DatePickerFooter {...props} {...{ show, setShow }} />
+                </div>
+              )}
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 DatePicker.propTypes = {
-    // toggler
-    // showBody
-    // renderToggler TogglerComponent renderFooter FooterComponent
+  // toggler
+  // showBody
+  // renderToggler TogglerComponent renderFooter FooterComponent
 };
 
 /**
@@ -206,15 +201,15 @@ DatePicker.propTypes = {
  */
 
 const Weekdays = (props) => (
-    <div id={props.id} className="Weekdays">
-        {WEEKDAYS.map((day) => {
-            return (
-                <div className="Weekday" key={day.index}>
-                    {day[`short`]}
-                </div>
-            );
-        })}
-    </div>
+  <div id={props.id} className="Weekdays">
+    {WEEKDAYS.map((day) => {
+      return (
+        <div className="Weekday" key={day.index}>
+          {day[`short`]}
+        </div>
+      );
+    })}
+  </div>
 );
 
 /**
@@ -224,82 +219,82 @@ const Weekdays = (props) => (
  */
 
 const PrevButton = (props) => {
-    const { goPrevWeek, goPrevMonth } = useContext(CalendarCtx);
-    const { view = isRequired("view") } = props;
+  const { goPrevWeek, goPrevMonth } = useContext(CalendarCtx);
+  const { view = isRequired('view') } = props;
 
-    const goPrev = view === "week" ? goPrevWeek : goPrevMonth;
+  const goPrev = view === 'week' ? goPrevWeek : goPrevMonth;
 
-    return (
-        <button className="btn icon-btn btn-prev" onClick={goPrev}>
-            <ChevronLeft className="icon" />
-        </button>
-    );
+  return (
+    <button className="btn icon-btn btn-prev" onClick={goPrev}>
+      <ChevronLeft className="icon" />
+    </button>
+  );
 };
 
 const NextButton = (props) => {
-    const { goNextWeek, goNextMonth } = useContext(CalendarCtx);
-    const { view = isRequired("view") } = props;
+  const { goNextWeek, goNextMonth } = useContext(CalendarCtx);
+  const { view = isRequired('view') } = props;
 
-    const goNext = view === "week" ? goNextWeek : goNextMonth;
+  const goNext = view === 'week' ? goNextWeek : goNextMonth;
 
-    return (
-        <button className="btn icon-btn btn-next" onClick={goNext}>
-            <ChevronRight className="icon" />
-        </button>
-    );
+  return (
+    <button className="btn icon-btn btn-next" onClick={goNext}>
+      <ChevronRight className="icon" />
+    </button>
+  );
 };
 
 const Title = (props) => {
-    const { curDate, curWeek } = useContext(CalendarCtx);
-    const { view = isRequired("view") } = props;
+  const { curDate, curWeek } = useContext(CalendarCtx);
+  const { view = isRequired('view') } = props;
 
-    if (!isDate(curDate)) return null;
+  if (!isDate(curDate)) return null;
 
-    let format = { month: "long", year: "numeric" };
-    let comp;
+  let format = { month: 'long', year: 'numeric' };
+  let comp;
 
-    switch (view) {
-        case "week":
-            const { firstDate, lastDate } = curWeek;
-            format = { day: "numeric", month: "short" };
-            comp = (
-                <>
-                    {isDate(firstDate) && <span>{formatDate(firstDate, format)}</span>}
-                    {isDate(lastDate) && (
-                        <span>
-                            {" - "}
-                            {formatDate(lastDate, { ...format, year: "numeric" })}
-                        </span>
-                    )}
-                </>
-            );
-            break;
+  switch (view) {
+    case 'week':
+      const { firstDate, lastDate } = curWeek;
+      format = { day: 'numeric', month: 'short' };
+      comp = (
+        <>
+          {isDate(firstDate) && <span>{formatDate(firstDate, format)}</span>}
+          {isDate(lastDate) && (
+            <span>
+              {' - '}
+              {formatDate(lastDate, { ...format, year: 'numeric' })}
+            </span>
+          )}
+        </>
+      );
+      break;
 
-        default:
-            comp = formatDate(curDate, format);
-            break;
-    }
+    default:
+      comp = formatDate(curDate, format);
+      break;
+  }
 
-    return <div className="Title">{comp}</div>;
+  return <div className="Title">{comp}</div>;
 };
 
 const Toolbar = (props) => {
-    const { context = isRequired("Calendar context") } = props;
+  const { context = isRequired('Calendar context') } = props;
 
-    return (
-        <div className="Toolbar">
-            <select
-                name="view"
-                value={context.view}
-                onChange={({ target }) => {
-                    context.setView(target.value);
-                }}
-            >
-                <option value={"month"}>Month</option>
-                <option value={"week"}>Week</option>
-            </select>
-        </div>
-    );
+  return (
+    <div className="Toolbar">
+      <select
+        name="view"
+        value={context.view}
+        onChange={({ target }) => {
+          context.setView(target.value);
+        }}
+      >
+        <option value={'month'}>Month</option>
+        <option value={'week'}>Week</option>
+      </select>
+    </div>
+  );
 };
 
 /**
@@ -309,82 +304,82 @@ const Toolbar = (props) => {
  */
 
 export default function Calendar({ children, ...props }) {
-    const initialDate = isDate(props.initialDate) ? initialDate : new Date();
-    const [curDate, setCurDate] = useState(initialDate);
-    const { id, className, ...handlers } = props;
-    const { onWeekChange, onMonthChange, ...rest } = handlers;
+  const initialDate = isDate(props.initialDate) ? initialDate : new Date();
+  const [curDate, setCurDate] = useState(initialDate);
+  const { id, className, ...handlers } = props;
+  const { onWeekChange, onMonthChange, ...rest } = handlers;
 
-    const context = useMemo(() => {
-        const goPrevMonth = () => {
-            if (!isDate(curDate)) return;
-            const dt = new Date(curDate.setMonth(curDate.getMonth() - 1));
-            setCurDate(dt);
+  const context = useMemo(() => {
+    const goPrevMonth = () => {
+      if (!isDate(curDate)) return;
+      const dt = new Date(curDate.setMonth(curDate.getMonth() - 1));
+      setCurDate(dt);
 
-            if (onMonthChange) {
-                onMonthChange({ dt });
-            }
-        };
+      if (onMonthChange) {
+        onMonthChange({ dt });
+      }
+    };
 
-        const goNextMonth = () => {
-            if (!isDate(curDate)) return;
-            const dt = new Date(curDate.setMonth(curDate.getMonth() + 1));
-            setCurDate(dt);
+    const goNextMonth = () => {
+      if (!isDate(curDate)) return;
+      const dt = new Date(curDate.setMonth(curDate.getMonth() + 1));
+      setCurDate(dt);
 
-            if (onMonthChange) {
-                onMonthChange({ dt });
-            }
-        };
+      if (onMonthChange) {
+        onMonthChange({ dt });
+      }
+    };
 
-        const goPrevWeek = () => {
-            if (!isDate(curDate)) return;
+    const goPrevWeek = () => {
+      if (!isDate(curDate)) return;
 
-            const dt = new Date(curDate.setDate(curDate.getDate() - 7));
-            setCurDate(dt);
+      const dt = new Date(curDate.setDate(curDate.getDate() - 7));
+      setCurDate(dt);
 
-            if (onWeekChange) {
-                onWeekChange({ dt });
-            }
+      if (onWeekChange) {
+        onWeekChange({ dt });
+      }
 
-            // Todo: On month change
-        };
+      // Todo: On month change
+    };
 
-        const goNextWeek = () => {
-            if (!isDate(curDate)) return;
+    const goNextWeek = () => {
+      if (!isDate(curDate)) return;
 
-            const dt = new Date(curDate.setDate(curDate.getDate() + 7));
-            setCurDate(dt);
+      const dt = new Date(curDate.setDate(curDate.getDate() + 7));
+      setCurDate(dt);
 
-            if (onWeekChange) {
-                onWeekChange({ dt });
-            }
-        };
+      if (onWeekChange) {
+        onWeekChange({ dt });
+      }
+    };
 
-        const goToday = () => {
-            setCurDate(new Date());
-        };
+    const goToday = () => {
+      setCurDate(new Date());
+    };
 
-        return {
-            ...rest,
-            curDate,
-            setCurDate,
-            initialDate,
-            curWeek: getWeekDates(curDate),
-            curMonth: getMonthDates(curDate),
-            goToday,
-            goPrevWeek,
-            goNextWeek,
-            goPrevMonth,
-            goNextMonth,
-        };
-    }, [rest, curDate, setCurDate, initialDate]);
+    return {
+      ...rest,
+      curDate,
+      setCurDate,
+      initialDate,
+      curWeek: getWeekDates(curDate),
+      curMonth: getMonthDates(curDate),
+      goToday,
+      goPrevWeek,
+      goNextWeek,
+      goPrevMonth,
+      goNextMonth,
+    };
+  }, [rest, curDate, setCurDate, initialDate]);
 
-    return (
-        <CalendarCtx.Provider value={context}>
-            <div id={id} className={getClassName(["miq-calendar", className])}>
-                {children}
-            </div>
-        </CalendarCtx.Provider>
-    );
+  return (
+    <CalendarCtx.Provider value={context}>
+      <div id={id} className={getClassName(['miq-calendar', className])}>
+        {children}
+      </div>
+    </CalendarCtx.Provider>
+  );
 }
 
 // ToolBar
@@ -401,9 +396,9 @@ Calendar.Weekdays = Weekdays;
 Calendar.Month = Month;
 
 Calendar.propTypes = {
-    initialDate: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]),
-    children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
-    DateComponent: PropTypes.elementType,
-    onWeekChange: PropTypes.func,
-    onMonthChange: PropTypes.func,
+  initialDate: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]),
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
+  DateComponent: PropTypes.elementType,
+  onWeekChange: PropTypes.func,
+  onMonthChange: PropTypes.func,
 };
--- src/calendar/src/utils.jsx
+++ src/calendar/src/utils.jsx
@@ -1,73 +1,68 @@
-import { isDate } from "lodash";
-import { formatDateToStr } from "@miq/utils";
+import isDate from 'lodash.isdate';
+import { formatDateToStr } from '@miq/utils';
 
 export const dateIsToday = (date) => dateEquals(date, new Date());
 
 export const dateEquals = (date1, date2) => {
-    if (!isDate(date1) || !isDate(date2)) return false;
-    return formatDateToStr(date1) === formatDateToStr(date2);
+  if (!isDate(date1) || !isDate(date2)) return false;
+  return formatDateToStr(date1) === formatDateToStr(date2);
 };
 
 export const getMonthDates = (curDate) => {
-    if (!isValidDate(curDate)) {
-        curDate = new Date(); // Today
+  if (!isValidDate(curDate)) {
+    curDate = new Date(); // Today
+  }
+
+  const curYear = curDate.getFullYear();
+  const curMonth = curDate.getMonth();
+
+  const firstDate = new Date(curYear, curMonth, 1);
+  const lastDate = new Date(curYear, curMonth + 1, 0);
+  let datesByWeek = [];
+
+  let nextDate = new Date(firstDate);
+  for (let i = 0; nextDate.getMonth() === curMonth && nextDate.getFullYear() === curYear; i = i + 7) {
+    nextDate = new Date(firstDate);
+    nextDate.setDate(firstDate.getDate() + i);
+    const week = getWeekDates(nextDate);
+
+    if (week.firstDate > lastDate) {
+      break;
     }
 
-    const curYear = curDate.getFullYear();
-    const curMonth = curDate.getMonth();
+    datesByWeek = [...datesByWeek, week];
+  }
 
-    const firstDate = new Date(curYear, curMonth, 1);
-    const lastDate = new Date(curYear, curMonth + 1, 0);
-    let datesByWeek = [];
-
-    let nextDate = new Date(firstDate);
-    for (
-        let i = 0;
-        nextDate.getMonth() === curMonth && nextDate.getFullYear() === curYear;
-        i = i + 7
-    ) {
-        nextDate = new Date(firstDate);
-        nextDate.setDate(firstDate.getDate() + i);
-        const week = getWeekDates(nextDate);
-
-        if (week.firstDate > lastDate) {
-            break;
-        }
-
-        datesByWeek = [...datesByWeek, week];
-    }
-
-    return { weeks: datesByWeek, firstDate, lastDate };
+  return { weeks: datesByWeek, firstDate, lastDate };
 };
 
 export const getWeekDates = (currDate) => {
-    if (!isValidDate(currDate)) {
-        currDate = new Date(); // Today
-    }
+  if (!isValidDate(currDate)) {
+    currDate = new Date(); // Today
+  }
 
-    const offset = currDate.getDate() - currDate.getDay();
-    const firstDate = new Date(currDate);
-    firstDate.setDate(offset);
+  const offset = currDate.getDate() - currDate.getDay();
+  const firstDate = new Date(currDate);
+  firstDate.setDate(offset);
 
-    const dates = [];
-    for (let i = 0; i < 7; i++) {
-        const date = new Date(firstDate);
-        const nextDate = firstDate.getDate() + i;
-        date.setDate(nextDate);
-        dates.push(date);
-    }
-    return { dates: [...dates], firstDate, lastDate: dates.slice(-1)[0] };
+  const dates = [];
+  for (let i = 0; i < 7; i++) {
+    const date = new Date(firstDate);
+    const nextDate = firstDate.getDate() + i;
+    date.setDate(nextDate);
+    dates.push(date);
+  }
+  return { dates: [...dates], firstDate, lastDate: dates.slice(-1)[0] };
 };
 
-export const isValidDate = (date) =>
-    date && Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date);
+export const isValidDate = (date) => date && Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date);
 
 export const WEEKDAYS = [
-    { short: "Sun", long: "Sunday" },
-    { short: "Mon", long: "Monday" },
-    { short: "Tue", long: "Tuesday" },
-    { short: "Wed", long: "Wednesday" },
-    { short: "Thu", long: "Thursday" },
-    { short: "Fri", long: "Friday" },
-    { short: "Sat", long: "Saturday" },
+  { short: 'Sun', long: 'Sunday' },
+  { short: 'Mon', long: 'Monday' },
+  { short: 'Tue', long: 'Tuesday' },
+  { short: 'Wed', long: 'Wednesday' },
+  { short: 'Thu', long: 'Thursday' },
+  { short: 'Fri', long: 'Friday' },
+  { short: 'Sat', long: 'Saturday' },
 ].map((d, i) => ({ ...d, index: i }));
